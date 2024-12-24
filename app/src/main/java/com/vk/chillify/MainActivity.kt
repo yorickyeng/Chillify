36946bf872db8c527a4a281d582323e2160d79dc
yorickyeng <sultanovainur05@gmail.com>
2024-12-24 03:04:59 +0300

    v1.2 - Notifications and Settings screen

--- app/src/main/java/com/vk/chillify/MainActivity.kt
+++ app/src/main/java/com/vk/chillify/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
--- app/src/main/java/com/vk/chillify/presentation/templates/SongSection.kt
+++ app/src/main/java/com/vk/chillify/presentation/templates/SongSection.kt
@@ -1,9 +1,11 @@
 package com.vk.chillify.presentation.templates
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,31 +17,36 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun SongsSection(text: String, artistName: String, url: String) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
+    Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 6.dp)
     ) {
-        Text(
-            text = text,
-            color = Color.White,
-            fontSize = 19.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.weight(1f)
-        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 6.dp)
+        ) {
+            Text(
+                text = text,
+                color = Color.White,
+                fontSize = 19.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.weight(1f)
+            )
+        }
+        HorizontalFramesRow { SongCover(artistName, url) }
     }
-    HorizontalFramesRow { SongCover(artistName, url) }
 }
 
 @Composable
 fun HorizontalFramesRow(content: @Composable () -> Unit) {
-    Row(
+    LazyRow(
         horizontalArrangement = Arrangement.spacedBy(13.dp),
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 6.dp, 7.dp)
+            .padding(horizontal = 6.dp, vertical = 7.dp)
     ) {
-        repeat(2) { content() }
+        items(20) { content() }
     }
 }
